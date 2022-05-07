52cf873ac6c323e994ca58d9e1dcae4b6ad82764
ryann127 <ryann.goldberg127@gmail.com>
2022-05-07 12:01:39 -0600

    fixed routing in api routes. definied variables within POST. Notes now save and append to db.json

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -10,21 +10,23 @@ const { readFromFile, writeToFile,readAndAppend, uuid  } = require('../helpers/f
 //     })
 // })
 // saves notes and joins to db.json
-router.get("/api/notes", (req,res) => {
+router.get("/notes", (req,res) => {
     readFromFile('./db/db.json')
     .then((router) => 
     res.json(JSON.parse(router)))
 });
 // adds notes to db.json
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     let newNote = req.body;
     const note = {
-        title,
-        text, id: uuid(),
+        title: newNote.title,
+        text: newNote.text,
+        id: uuid,
     };
     console.log(uuid);
     readAndAppend(note, './db/db.json');
     res.json("your note is saved");
 })
+
 
 module.exports = router;
