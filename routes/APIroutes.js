29d0d54c5207012fd520c0d08e736e0321b05f3f
ryann127 <ryann.goldberg127@gmail.com>
2022-05-07 13:33:52 -0600

    cleaned up delete function and correct uuid from variable to function to randomize ids bw each note so that only a specific one would be deleted at a time

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,19 +1,12 @@
 const router = require('express').Router();
 
-const { readFromFile, writeToFile,readAndAppend, uuid  } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend, uuid } = require('../helpers/fsUtils');
 
-// get /api/notes
-// router.get('/api/router', function (req, res) {
-//     readFileAsync("../db/db.json", "utf8").then(function (data) {
-//         router = [].concat(JSON.parse(data))
-//         res.json(router);
-//     })
-// })
 // saves notes and joins to db.json
-router.get("/notes", (req,res) => {
+router.get("/notes", (req, res) => {
     readFromFile('./db/db.json')
-    .then((router) => 
-    res.json(JSON.parse(router)))
+        .then((router) =>
+            res.json(JSON.parse(router)))
 });
 // adds notes to db.json
 router.post('/notes', (req, res) => {
@@ -21,20 +14,21 @@ router.post('/notes', (req, res) => {
     const note = {
         title: newNote.title,
         text: newNote.text,
-        id: uuid,
+        id: uuid(),
     };
-    console.log(uuid);
+    console.log(uuid());
     readAndAppend(note, './db/db.json');
     res.json("your note is saved");
 })
 
 router.delete('/notes/:id', (req, res) => {
     readFromFile('./db/db.json')
-    .then((result) => {
-        let dbParse = JSON.parse(result);
-        lastParse = dbParse.filter(result => result.id !== req.params.id);
-        writeToFile('./db/db.json', lastParse)
-    })
+        .then((journal) => {
+            let dbParse = JSON.parse(journal);
+            lastParse = dbParse.filter(journal => journal.id !== req.params.id);
+            writeToFile('./db/db.json', lastParse);
+            res.json(lastParse)
+        })
 })
 
 module.exports = router;
