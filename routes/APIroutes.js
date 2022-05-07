83ce36e34d41132ba13764e87689707aedf6829f
ryann127 <ryann.goldberg127@gmail.com>
2022-05-07 10:05:41 -0600

    working on uuid variable a bit. added a post function

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-
-
+// const uuid = Math.floor((1 + Math.random()) * 0x10000)
+//     .toString(16)
+//     .substring(1);
 // get /api/notes 
 // router.get('/api/notes', function (req, res) {
 //     readFileAsync("../db/db.json", "utf8").then(function (data) {
@@ -16,7 +17,14 @@ router.get("/api/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 // adds notes to db.json
-
+router.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+})
 
 
 
