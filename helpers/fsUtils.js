85be6ae4c024e114a81d4a2690bcf98926a9e846
ryann127 <ryann.goldberg127@gmail.com>
2022-05-07 11:11:50 -0600

    trying, yet again, to get my data to save to the webpage. API routes tweaking

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -21,4 +21,8 @@ const readAndAppend = (content, file) => {
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+const uuid = Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+module.exports = { readFromFile, writeToFile, readAndAppend, uuid };
--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,19 +1,15 @@
 const router = require('express').Router();
-const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const { readFromFile, writeToFile,readAndAppend  } = require('../helpers/fsUtils');
-const uuid = Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
-// get /api/notes 
-// router.get('/api/notes', function (req, res) {
+
+const { readFromFile, writeToFile,readAndAppend, uuid  } = require('../helpers/fsUtils');
+
+// get /api/notes
+// router.get('/api/router', function (req, res) {
 //     readFileAsync("../db/db.json", "utf8").then(function (data) {
-//         notes = [].concat(JSON.parse(data))
-//         res.json(notes);
+//         router = [].concat(JSON.parse(data))
+//         res.json(router);
 //     })
 // })
-//saves notes and joins to db.json
+// saves notes and joins to db.json
 router.get("/api/notes", (req,res) => {
     readFromFile('./db/db.json')
     .then((router) => 
@@ -22,14 +18,13 @@ router.get("/api/notes", (req,res) => {
 // adds notes to db.json
 router.post('/api/notes', (req, res) => {
     let newNote = req.body;
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-
-    newNotes.id = uuid.v4();
-    notes.push(newNotes);
-    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
-    res.json(notes);
+    const note = {
+        title,
+        text, id: uuid(),
+    };
+    console.log(uuid);
+    readAndAppend(note, './db/db.json');
+    res.json("your note is saved");
 })
-
-
 
 module.exports = router;
